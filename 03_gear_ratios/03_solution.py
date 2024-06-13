01562b4a49bc878fedc0d6b5e2da9c29e4c4c92a
Reza Saidafkan <93768131+rsaidafk@users.noreply.github.com>
2024-06-13 23:59:36 +0200

    refactored, brought concrete datatypes, got the correct answer

--- 03_gear_ratios/03_solution.py
+++ 03_gear_ratios/03_solution.py
@@ -1,6 +1,51 @@
 from pathlib import Path
-from typing import Tuple
-import functools
+from typing import Tuple, List
+from dataclasses import dataclass
+from enum import Enum
+from logging import getLogger, basicConfig, INFO, DEBUG
+
+logger = getLogger(__name__)
+
+
+class CheckType(Enum):
+    LEFT = "left"
+    RIGHT = "right"
+    UP = "up"
+    DOWN = "down"
+
+class Reason(Enum):
+    EDGE = "edge"
+    VALUE = "value"
+    
+@dataclass
+class CheckResult:
+    reason: Reason
+    value: str
+    verdict: bool
+    type: CheckType
+    
+    def __repr__(self) -> str:
+        return f"{self.type}: '{self.value}'"
+
+@dataclass
+class Token:
+    element: Tuple[int, int]
+    value: int
+    checks: List[CheckResult]
+    
+    def __repr__(self) -> str:
+        return f"{self.value}\t" + " \t".join([f"{check.type}: {check.value}" for check in self.checks if check.verdict])
+
+@dataclass
+class Partnumber:
+    tokens: List[Token]
+    
+    def __repr__(self) -> str:
+        return "\n".join([repr(token) for token in self.tokens])
+    
+    def value(self) -> int:
+        return int("".join([str(token.value) for token in self.tokens]))
+
 
 def _open_matrix(file_name):
     with open(Path(__file__).parent / file_name) as file:
@@ -10,122 +55,144 @@ def print_matrix(matrix):
     for row in matrix:
         print(row)
 
-
-def _parse_row_token(row, j):
-    token, tokens = [], []
+def _parse_row_partnumbers(row: str, i: int) -> List[Partnumber]:
+    partnumber: Partnumber
+    partnumbers: List[Partnumber] = []
+    
     on_a_token = False
-    for i, element in enumerate(row):
+    for j, element in enumerate(row):
         if not on_a_token:
-            token = []
+            partnumber = Partnumber(tokens=[])
         if element.isnumeric():
-            token.append((j, i, int(element)))
+            partnumber.tokens.append(Token(element=(i, j), value=int(element), checks=[]))
             on_a_token = True
         else:
             on_a_token = False
-            if token:
-                tokens.extend([token])
-    return tokens
+            if partnumber.tokens:
+                partnumbers.append(partnumber)
+    return partnumbers
 
 
-def _find_all_tokens(matrix):
-    all_tokens = []
-    for j, row in enumerate(matrix):
-        all_tokens.extend(_parse_row_token(row, j))        
-    return all_tokens
+def _find_matrix_partnumbers(matrix: List[List[str]]) -> List[Partnumber]:
+    matrix_partnumbers = []
+    for i, row in enumerate(matrix):
+        matrix_partnumbers.extend(_parse_row_partnumbers(row, i))
+    return matrix_partnumbers
 
-def _create_left_pad_element(element, matrix):
+def _create_left_pad_token(element, matrix):
     i, j = element[0], element[1]
-    return (i, j-1, matrix[i][j-1])
+    return Token(element=(i, j-1), value=matrix[i][j-1], checks=[])
 
-def _create_right_pad_element(element, matrix):
+def _create_right_pad_token(element, matrix):
     i, j = element[0], element[1]
-    return (i, j+1, matrix[i][j+1])
+    return Token(element=(i, j+1), value=matrix[i][j+1], checks=[])
 
-def _scan_token(token, matrix):
-    output_result = []
-    new_token = token.copy()
-    left_most_token = new_token.pop(0)
-    right_most_token = new_token.pop(-1)
-    middle_elements = new_token
+def _scan_partnumber(partnumber: Partnumber, matrix: List[List[str]]) -> Partnumber:
+    logger.debug(f"partnumber: {partnumber}")
+    new_tokens = partnumber.tokens.copy()
+    left_most_token = new_tokens.pop(0)
+    right_most_token = new_tokens.pop(-1)
+    middle_tokens = new_tokens
     
-    left_output_result = _check_left(left_most_token, matrix)
-    right_output_result = _check_right(right_most_token, matrix)
+    _check_left_token(left_most_token, matrix)
+    _check_right_token(right_most_token, matrix)
     
-    check_span = []
-    if left_output_result[1]:
-        left_pad_element = _create_left_pad_element(left_most_token, matrix)
-        check_span.append(left_pad_element)
+    check_span_tokens: List[Token] = []
+    if left_most_token.checks[0].reason == Reason.VALUE and not left_most_token.checks[0].verdict:
+        left_pad_element = _create_left_pad_token(left_most_token.element, matrix)
+        check_span_tokens.append(left_pad_element)
         
-    check_span.extend([left_most_token, *middle_elements, right_most_token])
+    check_span_tokens.extend([left_most_token, *middle_tokens, right_most_token])
+    if right_most_token.checks[0].reason == Reason.VALUE and not right_most_token.checks[0].verdict:
+        right_pad_element = _create_right_pad_token(right_most_token.element, matrix)
+        check_span_tokens.append(right_pad_element)
     
-    if right_output_result[1]:
-        right_pad_element = _create_right_pad_element(right_most_token, matrix)
-        check_span.append(right_pad_element)
+    for token in check_span_tokens:
+        _check_up_token(token, matrix)
+        _check_down_token(token, matrix)
+        
+    logger.debug("check_span_tokens:")
+    logger.debug('\n'.join([repr(token) for token in check_span_tokens]))
+    
+    if _evaluate_partnumber(check_span_tokens):
+        return partnumber
+    
 
-    up_down_outout_result = []
-    for element in check_span:
-        up_down_outout_result.append(_check_up(element, matrix))
-        up_down_outout_result.append(_check_down(element, matrix))
-    
-    output_result.insert(0, left_output_result)
-    output_result.extend(up_down_outout_result)
-    output_result.insert(-1, right_output_result)
-    print("token: ", token, " ".join([f"{o_r[0]}: {o_r[1]}\t" for o_r in output_result]))
-    
-    return output_result
-
-def _check_left(token, matrix) -> Tuple[str, str, bool]:
-    i, j = token[0], token[1]
+def _check_left_token(token: Token, matrix: List[List[str]]) -> None:
+    i, j = token.element[0], token.element[1]
     if j == 0:
-        return "'left edge'", None, False
+        token.checks.append(CheckResult(reason=Reason.EDGE, value=None, verdict=False, type=CheckType.LEFT))
     else:
-        left_element = matrix[i][j-1]
-        return f"'left'", left_element, _check_token(i, j-1, matrix)
+        left_element = (i, j-1)
+        value = matrix[i][j-1]
+        result = _check_element(left_element, matrix)
+        if not result:
+            token.checks.append(CheckResult(reason=Reason.VALUE, value=value, verdict=False, type=CheckType.LEFT))
+        else:
+            token.checks.append(CheckResult(reason=Reason.VALUE, value=value, verdict=True, type=CheckType.LEFT))
 
-def _check_right(token, matrix) -> Tuple[str, str, bool]:
-    i, j = token[0], token[1]
+
+def _check_right_token(token: Token, matrix: List[List[str]]) -> None:
+    i, j = token.element[0], token.element[1]
     if j == len(matrix[0]) - 1:
-        return "'right edge'", None, False
+        token.checks.append(CheckResult(reason=Reason.EDGE, value=None, verdict=False, type=CheckType.RIGHT))
     else:
-        right_element = matrix[i][j+1]
-        return f"'right'", right_element, _check_token(i, j+1, matrix)
+        right_element = (i, j+1)
+        value = matrix[i][j+1]
+        result = _check_element(right_element, matrix)
+        if not result:
+            token.checks.append(CheckResult(reason=Reason.VALUE, value=value, verdict=False, type=CheckType.RIGHT))
+        else:
+            token.checks.append(CheckResult(reason=Reason.VALUE, value=value, verdict=True, type=CheckType.RIGHT))
 
-def _check_up(element, matrix) -> Tuple[str, str, bool]:
-    i, j = element[0], element[1]
+def _check_up_token(token: Token, matrix: List[List[str]]) -> None:
+    i, j = token.element[0], token.element[1]
     if i == 0:
-        return "'upper edge'", None, False
+        token.checks.append(CheckResult(reason=Reason.EDGE, value=None, verdict=False, type=CheckType.UP))
     else:
-        upper_element = matrix[i-1][j]
-        return f"'up'", upper_element, _check_token(i-1, j, matrix)
-
-def _check_down(token, matrix) -> Tuple[str, str, bool]:
-    i, j = token[0], token[1]
+        upper_element = (i-1, j)
+        value = matrix[i-1][j]
+        result = _check_element(upper_element, matrix)
+        if not result:
+            token.checks.append(CheckResult(reason=Reason.VALUE, value=value, verdict=False, type=CheckType.UP))
+        else:
+            token.checks.append(CheckResult(reason=Reason.VALUE, value=value, verdict=True, type=CheckType.UP))
+    
+def _check_down_token(token: Token, matrix: List[List[str]]) -> None:
+    i, j = token.element[0], token.element[1]
     if i == len(matrix) - 1:
-        return "'lower edge'", None, False
+        token.checks.append(CheckResult(reason=Reason.EDGE, value=None, verdict=False, type=CheckType.DOWN))
     else:
-        lower_element = matrix[i+1][j]
-        return f"'down'", lower_element, _check_token(i+1, j, matrix)
+        lower_element = (i+1, j)
+        value = matrix[i+1][j]
+        result = _check_element(lower_element, matrix)
+        if not result:
+            token.checks.append(CheckResult(reason=Reason.VALUE, value=value, verdict=False, type=CheckType.DOWN))
+        else:
+            token.checks.append(CheckResult(reason=Reason.VALUE, value=value, verdict=True, type=CheckType.DOWN))
 
+def _check_element(element, matrix):
+    i, j = element[0], element[1]
+    value = matrix[i][j]
+    if value == ".":
+        return False
+    return value
 
-def _check_token(i, j, matrix):
-    return matrix[i][j] == "."
-
-def check_token(token, output_result):
-    if any([not o_r[2] for o_r in output_result]):
-        return int("".join([str(element[2]) for element in token]))
+def _evaluate_partnumber(check_span_tokens: List[Token]):
+    return [token for token in check_span_tokens if any([check.verdict for check in token.checks])]
     
 
 if __name__ == "__main__":
+    basicConfig(level=INFO)
     matrix = _open_matrix("Example3.txt")
-    all_tokens = _find_all_tokens(matrix)
     print_matrix(matrix)
-    output_result = []
-    for token in all_tokens:
-        checked_elements = _scan_token(token, matrix)
-        if true_token := check_token(token, checked_elements):
-            output_result.append(true_token)
+    all_partnumbers = _find_matrix_partnumbers(matrix)
+    all_checked_partnumbers = []
+    for partnumber in all_partnumbers:
+        if checked_partnumber := _scan_partnumber(partnumber, matrix):
+            logger.debug(f"checked_partnumber {checked_partnumber}")
+            all_checked_partnumbers.append(checked_partnumber.value())
         
-        
-    print("output_result: ", sum(output_result))
+    logger.info(f"output_result: {sum(all_checked_partnumbers)}")
     
     

62716276f43f403ca8868d74ecce5535a97b7103
Reza Saidafkan <93768131+rsaidafk@users.noreply.github.com>
2024-06-14 16:53:33 +0200

    refactored

--- 03_gear_ratios/03_solution.py
+++ 03_gear_ratios/03_solution.py
@@ -2,31 +2,28 @@ from pathlib import Path
 from typing import Tuple, List
 from dataclasses import dataclass
 from enum import Enum
-from logging import getLogger, basicConfig, INFO, DEBUG
+from logging import getLogger
 import re
 
 logger = getLogger(__name__)
 
 
-class CheckType(Enum):
+class TokenType(Enum):
     LEFT = "left"
     RIGHT = "right"
     UP = "up"
     DOWN = "down"
-
-class Reason(Enum):
-    EDGE = "edge"
-    VALUE = "value"
+    NORMAL = "normal"
     
+
 @dataclass
 class CheckResult:
-    reason: Reason
+    token_type: TokenType
     value: str
     verdict: bool
-    type: CheckType
     
     def __repr__(self) -> str:
-        return f"{self.type}: '{self.value}'"
+        return f"{self.token_type} '{self.value}'"
 
 @dataclass
 class Token:
@@ -36,8 +33,9 @@ class Token:
     
     def __repr__(self) -> str:
         if VERBOSE:
-            return f"{self.value}\t" + " \t".join([f"{check.type}: {check.value}" for check in self.checks if check.verdict])
-        return f"{self.value}\t"
+            if checks := [f"{check}" for check in self.checks]:
+                return f"[{self.value} - {' '.join(checks)}]"
+        return f"{self.value}"
 
 @dataclass
 class Partnumber:
@@ -45,11 +43,14 @@ class Partnumber:
     
     def __repr__(self) -> str:
         if VERBOSE:
-            return "\n".join([repr(token) for token in self.tokens])
-        return str(self.value())
-    
+            return "".join([repr(token) for token in self.tokens]) + "\t" + str(self.tokens[0].element) + "\t" + str(self)
+        return "".join([repr(token) for token in self.tokens])
+        
     def value(self) -> int:
         return int("".join([str(token.value) for token in self.tokens]))
+    
+    def __str__(self) -> str:
+        return str(self.value())
 
 
 def _open_matrix(file_name):
@@ -71,31 +72,32 @@ def _create_right_pad_token(element, matrix):
     return Token(element=(i, j+1), value=matrix[i][j+1], checks=[])
 
 def _scan_partnumber(partnumber: Partnumber, matrix: List[List[str]]) -> Partnumber:
-    logger.debug(f"partnumber: {partnumber}")
-    new_tokens = partnumber.tokens.copy()
-    left_most_token = new_tokens.pop(0)
+    slice_partnumber = partnumber.tokens.copy()
+    
+    left_most_token = slice_partnumber.pop(0)
     _check_left_token(left_most_token, matrix)
     
+    # check if the new partnumber has any 'right' tokens left
     right_most_token: Token = None
-    if new_tokens:
-        right_most_token = new_tokens.pop(-1)
+    if slice_partnumber:
+        right_most_token = slice_partnumber.pop(-1)
     else:
         right_most_token = left_most_token
     _check_right_token(right_most_token, matrix)
         
-    middle_tokens = new_tokens
+    middle_tokens = slice_partnumber
     
     check_span_tokens: List[Token] = []
     
     check_span_tokens.extend([left_most_token, *middle_tokens])
-    if not left_most_token.checks[0].verdict and left_most_token.checks[0].reason != Reason.EDGE:
+    if left_most_token.checks[0].token_type == TokenType.NORMAL:
         left_pad_element = _create_left_pad_token(left_most_token.element, matrix)
         check_span_tokens.insert(0, left_pad_element)
         
     if right_most_token:
         check_span_tokens.append(right_most_token)
         
-    if not right_most_token.checks[0].verdict and right_most_token.checks[0].reason != Reason.EDGE:
+    if right_most_token.checks[0].token_type == TokenType.NORMAL:
         right_pad_element = _create_right_pad_token(right_most_token.element, matrix)
         check_span_tokens.append(right_pad_element)
     
@@ -103,9 +105,6 @@ def _scan_partnumber(partnumber: Partnumber, matrix: List[List[str]]) -> Partnum
         _check_up_token(token, matrix)
         _check_down_token(token, matrix)
         
-    logger.debug("check_span_tokens:")
-    logger.debug('\n'.join([repr(token) for token in check_span_tokens]))
-    
     if _evaluate_partnumber(check_span_tokens):
         return partnumber
     
@@ -113,55 +112,54 @@ def _scan_partnumber(partnumber: Partnumber, matrix: List[List[str]]) -> Partnum
 def _check_left_token(token: Token, matrix: List[List[str]]) -> None:
     i, j = token.element[0], token.element[1]
     if j == 0:
-        token.checks.append(CheckResult(reason=Reason.EDGE, value=None, verdict=False, type=CheckType.LEFT))
+        token.checks.append(CheckResult(token_type=TokenType.LEFT, value=None, verdict=False))
     else:
         left_element = (i, j-1)
         value = matrix[i][j-1]
         result = _check_element(left_element, matrix)
         if not result:
-            token.checks.append(CheckResult(reason=Reason.VALUE, value=value, verdict=False, type=CheckType.LEFT))
+            token.checks.append(CheckResult(token_type=TokenType.NORMAL, value=value, verdict=False))
         else:
-            token.checks.append(CheckResult(reason=Reason.VALUE, value=value, verdict=True, type=CheckType.LEFT))
-
+            token.checks.append(CheckResult(token_type=TokenType.NORMAL, value=value, verdict=True))
 
 def _check_right_token(token: Token, matrix: List[List[str]]) -> None:
     i, j = token.element[0], token.element[1]
     if j == len(matrix[0]) - 1:
-        token.checks.append(CheckResult(reason=Reason.EDGE, value=None, verdict=False, type=CheckType.RIGHT))
+        token.checks.append(CheckResult(token_type=TokenType.RIGHT, value=None, verdict=False))
     else:
         right_element = (i, j+1)
         value = matrix[i][j+1]
         result = _check_element(right_element, matrix)
         if not result:
-            token.checks.append(CheckResult(reason=Reason.VALUE, value=value, verdict=False, type=CheckType.RIGHT))
+            token.checks.append(CheckResult(token_type=TokenType.NORMAL, value=value, verdict=False))
         else:
-            token.checks.append(CheckResult(reason=Reason.VALUE, value=value, verdict=True, type=CheckType.RIGHT))
+            token.checks.append(CheckResult(token_type=TokenType.NORMAL, value=value, verdict=True))
 
 def _check_up_token(token: Token, matrix: List[List[str]]) -> None:
     i, j = token.element[0], token.element[1]
     if i == 0:
-        token.checks.append(CheckResult(reason=Reason.EDGE, value=None, verdict=False, type=CheckType.UP))
+        token.checks.append(CheckResult(token_type=TokenType.UP, value=None, verdict=False))
     else:
         upper_element = (i-1, j)
         value = matrix[i-1][j]
         result = _check_element(upper_element, matrix)
         if not result:
-            token.checks.append(CheckResult(reason=Reason.VALUE, value=value, verdict=False, type=CheckType.UP))
+            token.checks.append(CheckResult(token_type=TokenType.NORMAL, value=value, verdict=False))
         else:
-            token.checks.append(CheckResult(reason=Reason.VALUE, value=value, verdict=True, type=CheckType.UP))
+            token.checks.append(CheckResult(token_type=TokenType.NORMAL, value=value, verdict=True))
     
 def _check_down_token(token: Token, matrix: List[List[str]]) -> None:
     i, j = token.element[0], token.element[1]
     if i == len(matrix) - 1:
-        token.checks.append(CheckResult(reason=Reason.EDGE, value=None, verdict=False, type=CheckType.DOWN))
+        token.checks.append(CheckResult(token_type=TokenType.DOWN, value=None, verdict=False))
     else:
         lower_element = (i+1, j)
         value = matrix[i+1][j]
         result = _check_element(lower_element, matrix)
         if not result:
-            token.checks.append(CheckResult(reason=Reason.VALUE, value=value, verdict=False, type=CheckType.DOWN))
+            token.checks.append(CheckResult(token_type=TokenType.NORMAL, value=value, verdict=False))
         else:
-            token.checks.append(CheckResult(reason=Reason.VALUE, value=value, verdict=True, type=CheckType.DOWN))
+            token.checks.append(CheckResult(token_type=TokenType.NORMAL, value=value, verdict=True))
 
 def _check_element(element, matrix):
     i, j = element[0], element[1]
@@ -176,15 +174,17 @@ def _evaluate_partnumber(check_span_tokens: List[Token]):
 
 if __name__ == "__main__":
     import pdb
+    from logging import basicConfig, DEBUG, INFO
     basicConfig(level=DEBUG)
-    VERBOSE = False
+    VERBOSE = True
     matrix = _open_matrix("input.txt")
-    all_partnumbers = _find_matrix_partnumbers([matrix[0]])
+    all_partnumbers = _find_matrix_partnumbers(matrix)
     all_checked_partnumbers = []
     for partnumber in all_partnumbers:
         if checked_partnumber := _scan_partnumber(partnumber, matrix):
-            logger.debug(f"checked_partnumber {checked_partnumber}")
+            logger.debug(f"\t {repr(checked_partnumber)}")
             all_checked_partnumbers.append(checked_partnumber.value())
-    print(all_checked_partnumbers, sum(all_checked_partnumbers))
+    logger.info(all_checked_partnumbers)
+    logger.info(sum(all_checked_partnumbers))
     
     
